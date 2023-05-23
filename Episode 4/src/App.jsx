4ebb8d335d96823bf41c936747d59d94e917c5f6
R3MO DAS <remodas7774@gmail.com>
2023-05-24 00:27:14 +0530

    react lifecycle and useEffect

---
+++ Episode 4/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import Text from "./Text"
+
+function App() {
+  const [ShowText, setShowText] = useState(false);
+
+  return (
+    <>
+      <div className="App">
+        <button onClick={() => setShowText(!ShowText)}>Show Text</button>
+        {ShowText && <Text />}
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Episode 4/src/Text.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Text() {
+  return (
+    <h1>This works !!!</h1>
+  )
+}
+
+export default Text
